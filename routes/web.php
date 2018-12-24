1f102c7ed0d94c0999f5cdd77973766a010732aa
likebestupid <likebestupid@gmail.com>
2018-12-24 14:30:41 +0800

    Revert "Error"
    This reverts commit bce8485e52ef7b2039b9d82860a71cdd4ec0cbfa.

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('posts', ['as' => 'posts.index', 'uses' => 'PostsController@index']);
-Route::get('shop', ['as' => 'posts.show', 'uses' => 'PostsController@show']);
-Route::get('vegetables', ['as' => 'posts.shows', 'uses' => 'PostsController@shows']);
-Route::get('news', ['as' => 'posts.news', 'uses' => 'PostsController@news']);
+Route::get('posts', ['as' => 'posts.index',   'uses' => 'PostsController@index']);
+Route::get('shop',  ['as' => 'posts.show',    'uses' => 'PostsController@show']);
+Route::get('vegetables',  ['as' => 'posts.shows',    'uses' => 'PostsController@shows']);
+Route::get('about',    ['as' => 'posts.about',   'uses' => 'PostsController@about']);
+Route::get('contact',  ['as' => 'posts.contact', 'uses' => 'PostsController@contact']);
+Route::get('news',  ['as' => 'posts.news', 'uses' => 'PostsController@news']);
