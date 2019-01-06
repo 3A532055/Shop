dd3ec6e08bda10ea485a926f5cd9086ba71d4ae8
3A532089 <likebestupid@gmail.com>
2019-01-06 23:43:43 +0800

    商店頁面完成

--- app/Http/Controllers/ShowShopInfo.php
+++ app/Http/Controllers/ShowShopInfo.php
@@ -4,18 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Shop;
 use App\Price;
+use App\Shop;
+use DB;
 
 class ShowShopInfo extends Controller
 {
     public function index($id){
-        $shopinfo=Shop::where('id',$id)->get();
+        $shopinfo=Shop::where('id',$id)->first();
         $data=['shops'=>$shopinfo];
-        $showprice=Price::where('s_id',$id)->get();
-        $data=['price'=>$showprice];
 
-        return view('shopinfo',$data);
+        $info=DB::table('price')
+        ->join('shops','price.s_id','=','shops.id')
+        ->join('vegetables','price.v_id','=','vegetables.id')
+        ->where('s_id',$id)
+        ->select('vegetables.name','price')
+        ->get();
+        $data2=['price'=>$info];
+
+        return view('shopinfo',$data,$data2);
     }
 
     public function showprice(){
--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -27,14 +27,14 @@
             @foreach($shops as $shopshow)
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="post-preview">
-                    <a href="">
+                    <a href="{{route('Show.Shop', $shopshow->id)}}">
                         <h2 class="post-title">
                             {{$shopshow -> name}}
                         </h2>
                     </a>    
                     <i class="fa fa-map-marker ">
                     <tr class="post-meta">
-                    <a href="https://www.google.com.tw/maps/place/%E5%9C%8B%E6%B3%B0%E4%B8%96%E8%8F%AF%E9%8A%80%E8%A1%8CATM(%E5%85%A8%E8%81%AF-%E5%A4%AA%E5%B9%B3%E4%B8%AD%E5%B9%B3%E5%BA%97)/@24.1436697,120.7340437,21z/data=!4m12!1m6!3m5!1s0x346922d1ffee7fdf:0x968bd2ea00072fd6!2z5YWo6IGv56aP5Yip5Lit5b-DIOWkquW5s-S4reW5s-W6lw!8m2!3d24.12287!4d120.717807!3m4!1s0x3469234aad307bef:0xaf7f3f3acfcb6efc!8m2!3d24.1436409!4d120.7340967" target="_blank">
+                    <a href="" target="_blank">
                         {{$shopshow -> location}}</a></tr>
                     </i>
                     </div>
